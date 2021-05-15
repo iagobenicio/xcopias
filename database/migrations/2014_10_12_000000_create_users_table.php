601dcb60abea09f084c12ea5df52596699774196
Iago Benicio <iagobensen@gmail.com>
2021-05-15 19:46:52 -0300

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->integer('type');
             $table->integer('copiasmes');
-            $table->integer('copiasrestante')
+            $table->integer('copiasrestante');
             $table->rememberToken();
             $table->timestamps();
         });
